034f7b9a6ca2d5fdfa5f85fbeac8ebc74dc99086
Pakhi Sinha <pakhisinha07@gmail.com>
2026-06-03 02:47:01 -0700

    update: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,15 @@
-import { useLocation, Link } from "react-router-dom";
-import "../styles/components/_navbar.scss"
+import { NavLink } from "react-router-dom";
+import "../styles/components/_navbar.scss";
 
-export function NavBar() {
-  const location = useLocation();
-  const isSidequests = location.pathname === "/sidequests";
-
+export function NavBar({ dark = false }: { dark?: boolean }) {
   return (
-    <nav className={`navbar ${isSidequests ? "navbar--sidequests" : "navbar--default"}`}>
-      <div className="navbar__logo">Pakhi Sinha</div>
+    <nav className={`navbar${dark ? " navbar--dark" : ""}`}>
+      <NavLink to="/" className="navbar__brand">Pakhi Sinha</NavLink>
       <ul className="navbar__links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/resume">Resume</Link></li>
-        <li><Link to="/sidequests">Sidequests</Link></li>
+        <li><NavLink to="/" end>Home</NavLink></li>
+        <li><a href="https://drive.google.com/file/d/1ml3ep4_F_veIqSVG771ucRiHMBmTcKoY/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
+        <li><a href="https://www.linkedin.com/in/pakhi-sinha-/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+        <li><a href="https://github.com/imakelaa" target="_blank" rel="noopener noreferrer">GitHub</a></li>
       </ul>
     </nav>
   );
