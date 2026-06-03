63239e9917474f57906cbde99536ff6627e6a5d6
Pakhi Sinha <pakhisinha07@gmail.com>
2026-06-03 02:54:02 -0700

    update: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,52 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Home from "./pages/Home/Home"
+import Home from "./pages/Home/Home";
 import { NavBar } from "./components/Navbar";
-import Resume from "../src/pages/ResumePage";
-import Sidequests from "./pages/Sidequests";
-import '../src/styles/base/_layout.scss'
+import Resume from "./pages/ResumePage";
+import CulturePage from "./pages/CulturePage";
+import WritingsPage from "./pages/WritingsPage";
+import WritingPostPage from "./pages/WritingPostPage";
+import PhotographyPage from "./pages/PhotographyPage";
+import "./styles/base/_layout.scss";
 import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
 
 function AppWrapper() {
   const location = useLocation();
+  const isHome = location.pathname === "/" || location.pathname === "";
+  const isLightPage = location.pathname.startsWith("/writings");
 
   useEffect(() => {
-    const pageClass = location.pathname === "/sidequests" ? "page-sidequests" : "page-default";
-    
-    document.body.className = pageClass;
-  }, [location]);
-  
+    if (location.pathname === "/writings") {
+      document.body.className = "page-writings";
+    } else if (location.pathname.startsWith("/contents")) {
+      document.body.className = "page-contents";
+    } else if (location.pathname.startsWith("/photography")) {
+      document.body.className = "page-photography";
+    } else {
+      document.body.className = "page-default";
+    }
+  }, [location.pathname]);
+
   return (
     <>
-      <NavBar />
-      <main className="content-wrapper">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/sidequests" element={<Sidequests />} />
-      </Routes>
+      <ScrollToTop />
+      <div style={{ position: isHome ? "relative" : "static" }}>
+        <NavBar dark={isLightPage} />
+      </div>
+      <main className={isHome ? "" : "content-wrapper"}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contents" element={<CulturePage />} />
+          <Route path="/writings" element={<WritingsPage />} />
+          <Route path="/writings/:slug" element={<WritingPostPage />} />
+          <Route path="/photography" element={<PhotographyPage />} />
+        </Routes>
       </main>
     </>
   );
@@ -37,4 +60,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
